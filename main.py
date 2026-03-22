ed5a90c1faee1e9f6b8d53dbdcf445c1e859a2c2
Yabin Reodica <reodica.asl@obmontessori.edu.ph>
2026-03-23 07:52:14 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from pyscript import display, document
+
+def check(e):
+    iu = document.getElementById("psw").value
+    ee = document.getElementById("letter").value
+    aia = document.getElementById("capital").value
+    ue = document.getElementById("number").value
+    e = document.getElementById("length").value
+
+
+   
+
+
+
